d1dc8d98172c3e6e29f061cefea8190b973e8cbf
mi2428 <mi2428782020@gmail.com>
2019-07-20 21:59:21 +0900

    Update health status and notify event when needed
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- src/sindan.py
+++ src/sindan.py
@@ -7,6 +7,11 @@ class HealthStatus:
     RED = 'RED'
 
 
+class AlertTrigger:
+    FAILURE = 'failure'
+    RECOVER = 'recover'
+
+
 class Watch:
     def __init__(self, watch_params=None, db_params=None, slack_params=None, debug=False):
         self.watch_period = watch_params['period']
@@ -19,41 +24,44 @@ class Watch:
         self.debug = debug
 
     def __update_health_status(self):
-        now_ts = round(time.time() * 1000)
-        time_range = '-{period}'.format(period=self.watch_period)
+        rt = []
         statuslst = []
-        for layer in ('datalink', 'interface', 'localnet', 'globalnet', 'dns', 'web'):
-            health_record = self.influxdb_cli.read_health_status(layer, time_range, limit=1)
-            last_st = health_record[0]['status']
+        time_range = '-{period}'.format(period=self.watch_period)
+        for layer in ['datalink', 'interface', 'localnet', 'globalnet', 'dns', 'web']:
+            last_st = self.influxdb_cli.read_health_status(layer, time_range, limit=1)[0]['status']
             result_failed = self.influxdb_cli.read_diagnosis_logs(layer, time_range, ['result'], ['fail'])
-            current_st = None
-
-            # 通知はメッセージをまとめたいのでバッチ処理する
             if len(result_failed) < self.threshold:
                 current_st = HealthStatus.GREEN
                 if last_st == HealthStatus.RED:
-                    self.__recover_notification()
+                    rt.append({
+                        'layer': layer,
+                        'trigger': AlertTrigger.RECOVER
+                    })
             else:
-                # キャンペーンUUIDと計測タイプのリストを取得，まとめてSlackに投稿
                 current_st = HealthStatus.RED
                 if last_st == HealthStatus.GREEN:
-                    self.__down_notification()
+                    rt.append({
+                        'layer': layer,
+                        'trigger': AlertTrigger.FAILURE,
+                        'ts': [r['_time'] for r in result_failed]
+                    })
             statuslst.append((layer, current_st))
         self.influxdb_cli.write_health_status(statuslst)
+        return rt
 
-    def __recover_notification(self):
-        pass
+    def __notification_on_failure(self, alertlst):
+        if not alertlst:
+            return
 
-    def __down_notification(self):
-        pass
+    def __notification_on_recover(self, alertlst):
+        if not alertlst:
+            return
 
     def run(self):
-        """ MEMO
-        方針: 機関においてしきい値以上の 'fail' レコードが存在する場合は HealthStatus を RED にする
-        1. 期間における 'fail' のレコードを全件取得し個数を数える
-        2. しきい値を超えている場合は HealthStatus を RED とし現在のタイムスタンプでレコードを追加
-          2-1. 超えていなければ HealthStatus を GREEN とし現在のタイムスタンプでレコードを追加
-        3. HealthStatus が変化するタイミングをトリガ
-          3-1. 通知送信
-        """
-        pass
+        while True:
+            rt = self.__update_health_status()
+            if not rt:
+                continue
+            self.__notification_on_failure([alert for alert in rt if alert['trigger'] == AlertTrigger.FAILURE])
+            self.__notification_on_recover([alert for alert in rt if alert['trigger'] == AlertTrigger.RECOVER])
+            time.sleep(self.watch_interval)
