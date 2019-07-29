d6e03f73480cf46bf8dd3232474a76e692f26acb
mi2428 <mi2428782020@gmail.com>
2019-07-29 23:31:20 +0900

    develop: 2019/07/29 23:31:20
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- src/proxy.py
+++ src/proxy.py
@@ -1,7 +1,14 @@
 import influxdb2
+from config import *
 from socketserver import ThreadingMixIn
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from http import HTTPStatus
 import json
+
+
+class ProxyEndpoint:
+    DiagnosisLog = '/sindan.log'
+    CampaignLog = '/sindan.log_campaign'
 
 
 class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
@@ -9,18 +16,50 @@ class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
 
 
 class RequestHandler(BaseHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        self.influxdb_cli = None  # load config.py to setup client
+        super().__init__(*args, **kwargs)
+
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
         self.wfile.write(b'It works!')
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        return
 
     def do_POST(self):
         bytes = int(self.headers['Content-Length'])
         rawdata = self.rfile.read(bytes)
         jbody = json.loads(rawdata.decode('utf-8'))
-        self.send_response(200)
+        if self.path == ProxyEndpoint.DiagnosisLog:
+            self.__diagnosis(jbody)
+            self.send_response(HTTPStatus.OK)
+            self.end_headers()
+            return
+        if self.path == ProxyEndpoint.CampaignLog:
+            self.__campaign(jbody)
+            self.send_response(HTTPStatus.OK)
+            self.end_headers()
+            return
+        self.send_response(HTTPStatus.BAD_REQUEST)
         self.end_headers()
+        return
+
+    def __diagnosis(self, jbody):
+        # process raw data
+        print(self.influxdb_cli)
+        print(jbody)  # debug
+        pass
+
+    def __campaign(self, jbody):
+        # process raw data
+        print(jbody)  # debug
+        pass
 
 
-server = ThreadingHTTPServer(('0.0.0.0', 8081), RequestHandler)
-server.serve_forever()
+class ProxyServer:
+    def __init__(self, ip='0.0.0.0', port=8081):
+        ThreadingHTTPServer((ip, port), RequestHandler).serve_forever()
+
+
+if __name__ == '__main__':
+    p = ProxyServer(ip='127.0.0.1')
