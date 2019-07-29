60890ce53ee14a49dd4f640ea1920a67c03aa9ad
mi2428 <mi2428782020@gmail.com>
2019-07-29 22:54:34 +0900

    develop: 2019/07/29 22:54:34
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- src/proxy.py
+++ src/proxy.py
@@ -1,8 +1,26 @@
 import influxdb2
-from bottle import route, run
-from bottle import post, get, request, response
+from socketserver import ThreadingMixIn
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
 
-if __name__ == '__main__':
-    ProxyServer(host='127.0.0.1', port=8081, debug=True)
+class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
+    pass
+
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'It works!')
+
+    def do_POST(self):
+        bytes = int(self.headers['Content-Length'])
+        rawdata = self.rfile.read(bytes)
+        jbody = json.loads(rawdata.decode('utf-8'))
+        self.send_response(200)
+        self.end_headers()
+
+
+server = ThreadingHTTPServer(('0.0.0.0', 8081), RequestHandler)
+server.serve_forever()
