3dc6f7eb061570fa5a330cf406b549af231cf865
mi2428 <mi2428782020@gmail.com>
2019-07-30 15:25:12 +0900

    develop: 2019/07/30 15:25:12
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

---
+++ src/config.py
@@ -0,0 +1,22 @@
+import yaml
+
+
+class ConfigParser:
+    def __init__(self, config='./config.yml'):
+        content = None
+        with open(config, 'r') as fd:
+            try:
+                content = yaml.safe_load(fd)
+            except yaml.YAMLError as e:
+                print(e)
+
+        self.Proxy_IP = content['proxy']['ip']
+        self.Proxy_Port = content['proxy']['port']
+        self.DB_Host = content['influxdb']['host']
+        self.DB_Organization = content['influxdb']['organization']
+        self.DB_Token = content['influxdb']['token']
+        self.DB_DiagnosisBucket = content['influxdb']['diagnosis_bucket']
+        self.DB_HealthCheckBucket = content['influxdb']['healthcheck_bucket']
+
+
+Config = ConfigParser()
--- src/proxy.py
+++ src/proxy.py
@@ -1,29 +1,9 @@
+from config import Config
 import influxdb2
 from socketserver import ThreadingMixIn
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from http import HTTPStatus
-import yaml
 import json
-
-
-class ProxyConfig:
-    def __init__(self, config='./config.yml'):
-        content = None
-        with open(config, 'r') as fd:
-            try:
-                content = yaml.safe_load(fd)
-            except yaml.YAMLError as e:
-                print(e)
-        self.Proxy_IP = content['proxy']['ip']
-        self.Proxy_Port = content['proxy']['port']
-        self.DB_Host = content['influxdb']['host']
-        self.DB_Organization = content['influxdb']['organization']
-        self.DB_Token = content['influxdb']['token']
-        self.DB_DiagnosisBucket = content['influxdb']['diagnosis_bucket']
-        self.DB_HealthCheckBucket = content['influxdb']['healthcheck_bucket']
-
-
-Config = ProxyConfig()
 
 
 class ProxyEndpoint:
